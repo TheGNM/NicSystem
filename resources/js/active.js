6dec1eed1b4881d5573db825e745bf6aab28a300
GNM <gamernoobmaster67@gmail.com>
2026-05-15 18:54:27 +0800

    Update active.js

--- resources/js/active.js
+++ resources/js/active.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const currentFile = window.location.pathname.split("/").pop();
+    const currentFile = window.location.pathname.split("/").pop();
 
-  const navLinks = document.querySelectorAll(".navbar ul li a");
+    const navLinks = document.querySelectorAll(".navbar ul li a");
 
-  navLinks.forEach(link => {
-    const linkFile = link.getAttribute("href").split("/").pop();
-    
-    if (linkFile === currentFile) {
-      link.classList.add("active");
-    }
-  });
+    navLinks.forEach(link => {
+        const linkFile = link.getAttribute("href").split("/").pop();
+        if (linkFile === currentFile) {
+        link.classList.add("active");
+        }
+    });
 });
